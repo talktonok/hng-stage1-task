9e4a56de3eeadba126dbd50c86207c27651cab14
Mansur <mansuribrahimnok@gmail.com>
2022-10-19 18:27:52 +0100

    functional CRUD endpoint

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server Started at ${3000}`)
 })
