3e48f781b41e5d9f6e4d3691cb5db1572f8b0dd5
Mansur <mansuribrahimnok@gmail.com>
2022-10-22 15:33:06 +0100

    Added gitignore

--- controller/company.js
+++ controller/company.js
@@ -1,24 +1,82 @@
 const { Model } = require('mongoose');
+const companyModel = require('../models/companyModel')
 
-
-const store =   (req, res) => {
-    const company = new Model({
+const store =   async (req, res) => {
+    const company = new companyModel({
       name : req.body.name,
       head_office: req.body.head_office,
       phone: req.body.phone,
       email: req.body.email,
       employees: req.body.employees,
       category: req.body.category,
-      description: req.body.description,
+      description: req.body.description
 
     
   })
 
+
   try {
-    const dataToSave = company.save();
-    res.status(200).json(dataToSave)
+    const companyToSave = await company.save();
+    res.status(200).json(companyToSave)
 }
 catch (error) {
     res.status(400).json({message: error.message})
 }
+}
+
+const getAll = async (req, res) => {
+    try{
+        const company = await companyModel.find();
+        res.json(company)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+const getById =async (req, res) => {
+    try{
+        const company = await companyModel.findById(req.params.id);
+        res.json(company)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const updatedCompany = await companyModel.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(updatedCompany)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const deleteCompany = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const company = await companyModel.findByIdAndDelete(id)
+        res.send(`Document with ${company.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+module.exports = 
+{
+    store,
+    getAll,
+    getById,
+    update,
+    deleteCompany
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,83 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const companyModel = require('../models/companyModel')
 
-router.post('/company', async (req, res) => {
-    const company = new companyModel({
-      name : req.body.name,
-      head_office: req.body.head_office,
-      phone: req.body.phone,
-      email: req.body.email,
-      employees: req.body.employees,
-      category: req.body.category,
-      description: req.body.description,
+const companyController = require('../controller/company')
 
-    
-  })
-
-  try {
-    const companyToSave = await company.save();
-    res.status(200).json(companyToSave)
-}
-catch (error) {
-    res.status(400).json({message: error.message})
-}
-});
+router.post('/company', companyController.store)
 
 
-
-router.get('/company', async (req, res) => {
-    try{
-        const company = await companyModel.find();
-        res.json(company)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-});
+router.get('/company', companyController.getAll);
 
 
 //Get by ID Method
-router.get('/company/:id', async (req, res) => {
-    try{
-        const company = await companyModel.findById(req.params.id);
-        res.json(company)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-});
+router.get('/company/:id', companyController.getById);
 
 //Update by ID Method
-router.patch('/company/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-
-        const updatedCompany = await companyModel.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(updatedCompany)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-});
+router.patch('/company/:id', companyController.update);
 
 
 //Delete by ID Method
-router.delete('/company/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const company = await companyModel.findByIdAndDelete(id)
-        res.send(`Document with ${company.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+router.delete('/company/:id', companyController.deleteCompany)
 
 
 module.exports = router;
